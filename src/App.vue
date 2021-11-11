58cb18bea7f79f318368da424e0c93373000363e
zhiqiang <1215017984@qq.com>
2021-11-11 19:46:19 +0800

    updata

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: your name
  * @Date: 2021-11-09 08:06:56
- * @LastEditTime: 2021-11-09 10:44:47
- * @LastEditors: your name
+ * @LastEditTime: 2021-11-11 19:32:09
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \vue小项目\question\src\App.vue
 -->
@@ -11,7 +11,12 @@
     <router-view/>
   </div>
 </template>
-
+<script>
+export default {
+  
+  
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-09 08:06:56
- * @LastEditTime: 2021-11-11 14:53:25
+ * @LastEditTime: 2021-11-11 19:44:20
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \vue小项目\question\src\router\index.js
@@ -14,22 +14,43 @@ import Item from '../pages/item/Item.vue'
 import Score from '../pages/score/Score.vue'
 Vue.use(VueRouter)
 
+// const routes = [
+//   {
+//     path: '/',
+//     redirect:'/home'
+//   },
+//   {
+//     path: '/home',
+//     component: Home,
+//   },
+//   {
+//     path:'/item',
+//     component:Item
+//   },
+//   {
+//     path:'/score',
+//     component:Score
+//   }
+// ]
+
 const routes = [
   {
-    path: '/',
-    redirect:'/home'
-  },
-  {
-    path: '/home',
-    component: Home,
-  },
-  {
-    path:'/item',
-    component:Item
-  },
-  {
-    path:'/score',
-    component:Score
+    path:'/',
+    component:App,
+    children:[
+    {
+      path:'/home',
+      component:Home
+    },
+    {
+      path:'/item',
+      component:Item
+    },
+    {
+      path:'/score',
+      component:Score
+    }
+  ]
   }
 ]
 
