3d017ec5df2d72a89d4631a28e280c6c5f1d588d
zhiqiang <1215017984@qq.com>
2021-11-11 20:15:50 +0800

    updata

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-09 08:06:56
- * @LastEditTime: 2021-11-11 19:44:20
+ * @LastEditTime: 2021-11-11 20:13:32
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \vue小项目\question\src\router\index.js
@@ -35,11 +35,11 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path:'/',
+    path:'',
     component:App,
     children:[
     {
-      path:'/home',
+      path:'',
       component:Home
     },
     {
@@ -55,7 +55,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
